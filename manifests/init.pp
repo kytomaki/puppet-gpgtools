daf0e7d171492f6e8df33f2849bea5e2b23dc628
Sean Knox <knoxville@gmail.com>
2014-09-24 22:03:16 -0700

    Give package proper name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # Install GPGTools.
 class gpgtools {
   package { 'GPGTools':
-    name     => 'Install.pkg',
+    name     => 'GPGTools.pkg',
     provider => 'pkgdmg',
     source   => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg',
   }
