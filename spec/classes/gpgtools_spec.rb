d717ebf0e8211c04d0c2073c9c8beaec440ef957
Georg Friedrich <georg@wobolando.com>
2015-02-18 13:37:05 +1100

    Update spec

--- spec/classes/gpgtools_spec.rb
+++ spec/classes/gpgtools_spec.rb
@@ -3,6 +3,6 @@ describe 'gpgtools' do
   it do
     should contain_class('gpgtools')
     should contain_package('GPGTools').with_provider('pkgdmg')
-    should contain_package('GPGTools').with_source('https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg')
+    should contain_package('GPGTools').with_source('https://releases.gpgtools.org/GPG_Suite-2015.02-b5-1161.dmg')
   end
 end
